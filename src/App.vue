7e443741db68c66891b1d4319478f9aada1744c8
Kalliu Brasil <kalliu.brasil@zrp.com.br>
2024-01-12 21:16:22 -0300

    chore: Componentizing tutorial steps and removing boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import AttributesBindings from './components/AttributesBindings.vue'
 
 const message = ref('Hello World!')
-const isRed = ref(true)
-const color = ref("green")
-
-const toggleRed = () => {
-  isRed.value = !isRed.value
-}
-
-const toggleColor = () => {
-  color.value = color.value === 'green' ? 'blue' : 'green'
-}
-
 </script>
 
 <template>
   <h1>{{message}}</h1>
 
   <div class="page-body">
-      <p>
-        <span
-          :title="message"
-        >
-          Hover your mouse over me for a few seconds to see my dynamically bound title!
-        </span>
-      </p>
-
-      <p :class="{ red: isRed }" @click="toggleRed">
-        This should be red... but click me to toggle it.
-      </p>
-
-      <p :style="{color}" @click="toggleColor">
-        This should be green, and should toggle between green and blue on click.
-      </p>
+      <AttributesBindings />
   </div>
 
 </template>
 
 <style scoped>
-.red {
-  color: red;
-}
-
 .page-body {
   display: flex;
   flex-direction: column;
---
+++ src/components/AttributesBindings.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const titleMessage = 'Hello World!'
+  const isRed = ref(true)
+  const color = ref('green')
+
+  const toggleRed = () => {
+    isRed.value = !isRed.value
+  }
+
+  const toggleColor = () => {
+    color.value = color.value === 'green' ? 'blue' : 'green'
+  }
+</script>
+
+<template>
+  <p>
+    <span :title="titleMessage">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+  </p>
+
+  <p :class="{ red: isRed }" @click="toggleRed">This should be red... but click me to toggle it.</p>
+
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
+</template>
+
+<style scoped>
+  .red {
+    color: red;
+  }
+</style>
